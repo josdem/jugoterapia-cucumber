eb2a333593b4cee879ff72ae67b75ab49e7e558f
josdem <joseluis.delacruz@gmail.com>
2018-06-16 12:03:05 -0400

    Moving to the right packages

--- src/main/java/com/jos/dem/jugoterapia/cucumber/JugoterapiaCucumberApplication.java
+++ src/main/java/com/jos/dem/jugoterapia/cucumber/JugoterapiaCucumberApplication.java
@@ -1,12 +1,13 @@
-package com.example.jugoterapiacucumber;
+package com.jos.dem.jugoterapia.cucumber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JugoterapiaCucumberApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }
