aaca0687023651b204b176cb429681576ffb7e2f
josdem <joseluis.delacruz@gmail.com>
2018-06-17 17:14:41 -0400

    Adding category model

---
+++ src/main/java/com/jos/dem/jugoterapia/cucumber/model/Category.java
@@ -0,0 +1,15 @@
+package com.jos.dem.jugoterapia.cucumber.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+  private Long id;
+  private String name;
+
+}
