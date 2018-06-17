5a83c4db9b0b688545f16e6c25becb0a0573ef01
josdem <joseluis.delacruz@gmail.com>
2018-06-17 17:18:25 -0400

    Adding category service interface

---
+++ src/main/java/com/jos/dem/jugoterapia/cucumber/service/CategoryService.java
@@ -0,0 +1,10 @@
+package com.jos.dem.jugoterapia.cucumber.service;
+
+import reactor.core.publisher.Flux;
+import com.jos.dem.jugoterapia.cucumber.model.Category;
+
+public interface CategoryService {
+
+  Flux<Category> getAll();
+
+}
