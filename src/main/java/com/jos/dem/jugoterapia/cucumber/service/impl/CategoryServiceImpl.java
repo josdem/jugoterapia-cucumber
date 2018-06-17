653a02245961b8a2a0e72fef363091b14a623f17
josdem <joseluis.delacruz@gmail.com>
2018-06-17 19:02:26 -0400

    Adding server url in properties

--- src/main/java/com/jos/dem/jugoterapia/cucumber/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/jos/dem/jugoterapia/cucumber/service/impl/CategoryServiceImpl.java
@@ -2,17 +2,29 @@ package com.jos.dem.jugoterapia.cucumber.service;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
+import javax.annotation.PostConstruct;
+
 import reactor.core.publisher.Flux;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.context.annotation.PropertySource;
 
 import com.jos.dem.jugoterapia.cucumber.model.Category;
 import com.jos.dem.jugoterapia.cucumber.service.CategoryService;
 
 @Service
+@PropertySource("classpath:application.properties")
 public class CategoryServiceImpl implements CategoryService {
 
-  private WebClient client = WebClient.create("http://jugoterapia.josdem.io/jugoterapia-server");
+  @Value("${server.url}")
+  private String serverBasedUrl;
+  private WebClient client;
+
+  @PostConstruct
+  public void setup(){
+    client = WebClient.create(serverBasedUrl);
+  }
 
   public Flux<Category> getAll() {
     return client.get()
