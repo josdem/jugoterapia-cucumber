594fbbf5ae80f9bbe85d628c066a14098de4e317
josdem <joseluis.delacruz@gmail.com>
2018-06-17 17:09:25 -0400

    Adding category integration test

---
+++ src/test/java/com/jos/dem/jugoterapia/cucumber/CategoryIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.jos.dem.jugoterapia.cucumber;
+
+import com.jos.dem.jugoterapia.cucumber.model.Category;
+import com.jos.dem.jugoterapia.cucumber.service.CategoryService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import reactor.core.publisher.Flux;
+
+@ContextConfiguration(classes = JugoterapiaCucumberApplication.class)
+@WebAppConfiguration
+public class CategoryIntegrationTest {
+
+  @Autowired
+  private CategoryService categoryService;
+
+  Flux<Category> getCategories() throws Exception {
+    return categoryService.getAll();
+  }
+
+}
