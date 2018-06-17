0a11bdb212daeb618da07fc0d0b63b10e003b233
josdem <joseluis.delacruz@gmail.com>
2018-06-17 17:26:24 -0400

    #2 Adding category service implementation

---
+++ src/main/java/com/jos/dem/jugoterapia/cucumber/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,24 @@
+package com.jos.dem.jugoterapia.cucumber.service;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+import reactor.core.publisher.Flux;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.jos.dem.jugoterapia.cucumber.model.Category;
+import com.jos.dem.jugoterapia.cucumber.service.CategoryService;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+  private WebClient client = WebClient.create("http://jugoterapia.josdem.io/jugoterapia-server");
+
+  Flux<Category> getAll() {
+    return client.get()
+      uri("/categories/").accept(APPLICATION_JSON)
+      .retrieve()
+      .bodyToFlux(Category.class);
+  }
+
+}
