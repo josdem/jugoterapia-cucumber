18d724af10034dff5aa15037fa97db052e98a0fc
josdem <joseluis.delacruz@gmail.com>
2018-06-22 19:35:23 -0400

    #3 Adding assert messages

--- src/test/java/com/jos/dem/jugoterapia/cucumber/BeverageGetByAttributeTest.java
+++ src/test/java/com/jos/dem/jugoterapia/cucumber/BeverageGetByAttributeTest.java
@@ -19,10 +19,10 @@ public class BeverageGetByAttributeTest extends BeverageIntegrationTest {
     Beverage beverage = getBeverageById(35L).block();
 
     assertAll("beverage",
-        () -> assertEquals(new Long(35L), beverage.getId()),
-        () -> assertEquals("Jugo nutritivo (Zanahoria)", beverage.getName()),
-        () -> assertEquals("4 Zanahorias,1 Tallo de apío,1 Pera,5 hojas de espinacas", beverage.getIngredients()),
-        () -> assertTrue(beverage.getRecipe().length() > 50));
+        () -> assertEquals(new Long(35L), beverage.getId(), "Should be 35 id"),
+        () -> assertEquals("Jugo nutritivo (Zanahoria)", beverage.getName(), "Should get complete beverage name"),
+        () -> assertEquals("4 Zanahorias,1 Tallo de apío,1 Pera,5 hojas de espinacas", beverage.getIngredients(), "Should get complete ingredients"),
+        () -> assertTrue(beverage.getRecipe().length() > 50), "Recipe must be larger than 50 characters");
   }
 
 }
