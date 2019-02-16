0e88b72a4547182c5fe0ff8b4cae64b3480fc4cf
josdem <joseluis.delacruz@gmail.com>
2019-02-16 14:35:01 -0500

    #4 Refactoring beverage scenarios

--- src/test/java/com/jos/dem/jugoterapia/cucumber/BeverageGetByAttributeTest.java
+++ src/test/java/com/jos/dem/jugoterapia/cucumber/BeverageGetByAttributeTest.java
@@ -6,17 +6,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.jos.dem.jugoterapia.cucumber.model.Beverage;
 
+import java.util.Date;
 import java.util.List;
 
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import reactor.core.publisher.Mono;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BeverageGetByAttributeTest extends BeverageIntegrationTest {
 
-  @Then("^the client receives a beverage by id$")
+  private Beverage beverage;
+  private Logger log = LoggerFactory.getLogger(this.getClass());
+
+  @When("I request beverage by id")
+  public void shouldRequestBeverageById() throws Exception {
+    log.info("Running: I request beverage by id at " + new Date());
+    beverage = getBeverageById(35L).block();
+  }
+
+  @Then("I validate beverage data$")
   public void shouldGetBeverageById() throws Exception {
-    Beverage beverage = getBeverageById(35L).block();
+    log.info("Running: I validate beverage data at " + new Date());
 
     assertAll("beverage",
         () -> assertEquals(new Long(35L), beverage.getId(), "Should be 35 id"),
--- src/test/resources/beverage.feature
+++ src/test/resources/beverage.feature
@@ -1,3 +1,4 @@
 Feature: I can get a beverage by id
-  Scenario: Client makes a call to get a beverage information based in its id
-    Then the client receives a beverage by id
+  Scenario: Client can get a beverage by id
+    When I request beverage by id
+    Then I validate beverage data
