e9b1eb1837ee375e1ebaf8384d1f0267f728f46f
josdem <joseluis.delacruz@gmail.com>
2019-02-16 11:15:12 -0500

    #4 Refactoring categories request

--- src/test/java/com/jos/dem/jugoterapia/cucumber/CategoryGetTest.java
+++ src/test/java/com/jos/dem/jugoterapia/cucumber/CategoryGetTest.java
@@ -8,17 +8,28 @@ import java.util.List;
 
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
 import reactor.core.publisher.Flux;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CategoryGetTest extends CategoryIntegrationTest {
 
   private List<Category> categories;
+  private Logger log = LoggerFactory.getLogger(this.getClass());
 
-  @Then("^the client receives categories$")
-  public void shouldCallCategories() throws Exception {
-    List<Category> categories = getCategories()
+  @When("I request categories")
+  public void shouldRequestCategories() throws Exception {
+    log.info("Running: I request categories at " + new Date());
+    categories = getCategories()
       .collectList()
       .block();
+  }
+
+  @Then("I validate categories")
+  public void shouldCallCategories() throws Exception {
+    log.info("Running: I validate categories at " + new Date());
 
     assertTrue(categories.size() == 4,  () -> "Should be 4 categories");
     assertTrue(categories.contains(new Category(1L, "Curativos")), () -> "Should contains Curativos category");
--- src/test/resources/category.feature
+++ src/test/resources/category.feature
@@ -1,3 +1,4 @@
 Feature: I can get categories
   Scenario: Client makes a call to list categories
-    Then the client receives categories
+    When I request categories
+    Then I validate categories
