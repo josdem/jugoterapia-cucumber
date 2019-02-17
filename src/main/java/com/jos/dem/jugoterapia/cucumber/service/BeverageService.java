6c682713abb01949c17b527830fc6f45e1db3679
josdem <joseluis.delacruz@gmail.com>
2019-02-17 08:54:21 -0500

    #5 Adding get beverage by keyword definition

--- src/main/java/com/jos/dem/jugoterapia/cucumber/service/BeverageService.java
+++ src/main/java/com/jos/dem/jugoterapia/cucumber/service/BeverageService.java
@@ -1,10 +1,13 @@
 package com.jos.dem.jugoterapia.cucumber.service;
 
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
 import com.jos.dem.jugoterapia.cucumber.model.Beverage;
 
 public interface BeverageService {
 
   Mono<Beverage> getById(Long id);
+  Flux<Beverage> getByKeyword(String keyword);
 
 }
